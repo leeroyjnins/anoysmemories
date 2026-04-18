fcc3c7d586f793dfe6ed808f00f23bd8a236d112
leeroyjnins <hcnekoudah@gmail.com>
2026-04-18 21:21:38 +0800

    date

--- src/components/QRDisplay.jsx
+++ src/components/QRDisplay.jsx
@@ -174,10 +174,10 @@ export default function QRDisplay() {
             <div className={styles.infoRow}>
               <div className={styles.infoIcon}>
                 <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                <span>Date/time</span>
+                <span>Date</span>
               </div>
               <div className={styles.infoDetails}>
-                Date and Time to be announced
+                April 9, 2026
               </div>
             </div>
           </div>
